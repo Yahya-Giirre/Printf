bd6fb1b63ddc9bdfdddc8c5894f3ec5b3730fe7c
Yahya-Giirre <90740531+Yahya-Giirre@users.noreply.github.com>
2022-01-31 17:50:40 +1030

    Update ft_uXxp.c

--- Printf_yg/ft_uXxp.c
+++ Printf_yg/ft_uXxp.c
@@ -11,6 +11,32 @@
 /* ************************************************************************** */
 #include "ft_printf.h"
 
+void	ft_hex(unsigned int x, int *len, char hex)
+{
+	char	str[25]
+	int	i;
+	char	*base_char;
+	
+	if(hex == X)
+		base_char = "0123456789ABCDEF";
+	else
+		base_char = "0123456789abcdef";
+	i = 0;
+	if (hex == 0)
+	{
+		ft_putchar_len('0', len);
+		return ;
+	}
+	while (hex != 0)
+	{
+		str[i] = base_char[hex % 16];
+		hex /= 16;
+		i++;
+	}
+	while (i--)
+		ft_putchar_len(str[i], len);
+}
+
 void	ft_u(unsigned int u, int *len)
 {
 	char	str[25];
